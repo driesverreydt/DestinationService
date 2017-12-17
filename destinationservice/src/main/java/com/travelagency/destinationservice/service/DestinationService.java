8c055558de138a8c8269128885f7d1feb2a67335
driesverreydt <driesvv@hotmail.com>
2017-12-17 21:56:38 +0100

    no produce

--- destinationservice/src/main/java/com/travelagency/destinationservice/service/DestinationService.java
+++ destinationservice/src/main/java/com/travelagency/destinationservice/service/DestinationService.java
@@ -2,17 +2,12 @@ package com.travelagency.destinationservice.service;
 
 import java.util.Map;
 
-import javax.ws.rs.Produces;
-
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import model.DestinationInfo;
 
-@Produces(APPLICATION_JSON)
 @RestController
 public class DestinationService {
 Map<String,DestinationInfo> destinationInfos;
